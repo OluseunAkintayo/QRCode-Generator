cde4b8fe529f702a82f1a2e6e3d00a0f67d63b08
oluseunakintayo <oluseun.akintayo@gmail.com>
2024-08-29 15:34:59 +0100

    Routine: Swagger config

--- Program.cs
+++ Program.cs
@@ -61,10 +61,10 @@ var app = builder.Build();
 app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
+// if (app.Environment.IsDevelopment()) {
   app.UseSwagger();
   app.UseSwaggerUI();
-}
+// }
 
 app.UseCors("AllowLocalOrigin");
 
